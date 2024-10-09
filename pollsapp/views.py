793c9f148ec88a7a8c438487636a4afb9c783df5
Kad <learning.aik@gmail.com>
2024-10-09 18:53:07 +0200

    Correcting issue in view vote

--- pollsapp/views.py
+++ pollsapp/views.py
@@ -34,13 +34,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print(request.POST)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
             request,
-            "polls/detail.html",
+            "pollsapp/detail.html",
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
