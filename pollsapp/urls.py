979daffca029ab9b9f49ec60f15a6a9e1742ffa2
Kad <learning.aik@gmail.com>
2024-10-10 11:39:01 +0200

    Fixing the was_released_recently_bug
    Revoming the leading slashes on routes urls and adding a trailing slash on base path utilzing include()

--- pollsapp/urls.py
+++ pollsapp/urls.py
@@ -6,11 +6,11 @@ app_name = "pollsapp"
 
 urlpatterns = [
     # ex: /polls/
-    path("/", views.index, name="index"),
+    path("", views.index, name="index"),
     # ex: /polls/5/
-    path("/<int:pk>/", views.QuestionDetailView.as_view(), name="detail"),
+    path("<int:pk>/", views.QuestionDetailView.as_view(), name="detail"),
     # ex: /polls/5/results/
-    path("/<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
-    path("/<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
